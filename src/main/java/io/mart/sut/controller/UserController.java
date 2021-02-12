98558317d6313b90e069ed122cf45cdccd87b551
Aleksandr Martiushov <aleksandr.martiushov@signavio.com>
2021-02-12 08:49:47 +0100

    create user controller

---
+++ src/main/java/io/mart/sut/controller/UserController.java
@@ -0,0 +1,32 @@
+package io.mart.sut.controller;
+
+import java.util.Collection;
+import javax.validation.Valid;
+import javax.validation.constraints.NotNull;
+
+import io.mart.sut.exception.BookNotFoundException;
+import io.mart.sut.model.Book;
+import io.mart.sut.model.User;
+import io.mart.sut.repository.BookRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/user")
+public class UserController {
+	
+	@PostMapping("/")
+	@ResponseStatus(HttpStatus.CREATED)
+	public User createUser(@NotNull @Valid @RequestBody final User user) {
+		return user;
+	}
+}
